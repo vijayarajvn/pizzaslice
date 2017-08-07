0bb8cbfb497e93c27536353aa728b79e75e40e24
Vijayaraj <vijayaraj@kefassoftwares.com>
2017-08-07 15:01:09 +0530

    Added basic functionalities to MySQLConnection

--- src/lib/Database/MySQLConnection.cpp
+++ src/lib/Database/MySQLConnection.cpp
@@ -2,7 +2,39 @@
 
 namespace PizzaSlice { namespace Database 
 {
+    void MySQLConnection::connect()
+    {
+        _mysql = mysql_init(NULL);
 
+        mysql_options(_mysql, MYSQL_OPT_RECONNECT, (void *)"1");
+        
+        std::string host = _config.get<std::string>("Pizzaslice.Database.host");
+        std::string user = _config.get<std::string>("Pizzaslice.Database.user");
+        std::string password = _config.get<std::string>("Pizzaslice.Database.password");
+        std::string db = _config.get<std::string>("Pizzaslice.Database.db");
+
+        if(mysql_real_connect(_mysql,
+                              host.c_str(),
+                              user.c_str(),
+                              password.c_str(),
+                              db.c_str(),
+                              _config.get<unsigned>("Pizzaslice.Database.port"),
+                              NULL,
+                              0) == NULL)
+        {
+            std::cout << "MySQL connection failed.";
+        }
+        else
+        {
+            std::cout << "MySQL connection succeedded.";
+        }
+    }
+
+    void MySQLConnection::disconnect()
+    {
+        std::cout << "Closing...";
+        mysql_close(_mysql);
+    }
 
 
 }}
--- src/lib/Database/MySQLConnection.h
+++ src/lib/Database/MySQLConnection.h
@@ -1,6 +1,10 @@
 #ifndef PIZZASLICE_DATABASE_MYSQLCONNECTION_H
 #define PIZZASLICE_DATABASE_MYSQLCONNECTION_H
 
+#include <iostream>
+#include <boost/property_tree/ptree.hpp>
+#include <mysql.h>
+
 #include "Connection.h"
 
 namespace PizzaSlice { namespace Database 
@@ -9,10 +13,25 @@ namespace PizzaSlice { namespace Database
 class MySQLConnection: public Connection
 {
 public:
+    MySQLConnection(const boost::property_tree::ptree& config_):
+        _config(config_)
+    {
+        connect();
+    }
 
-    
+    ~MySQLConnection()
+    {
+        disconnect();
+    }
+
+    void connect();
+    void disconnect();
+
+private:
+    boost::property_tree::ptree   _config;
+    MYSQL *_mysql = mysql_init(NULL);  
 
 };
-
+    
 }}
 #endif
